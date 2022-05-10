02c524f1381268b2a67a54fc391c61cdd15350b8
Arundhati36 <91960066+Arundhati36@users.noreply.github.com>
2022-05-10 20:42:15 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC7kyMsWTw5brBehSYc9Pf-g7rJhwOa1Is",
+    authDomain: "project94-b1972.firebaseapp.com",
+    databaseURL: "https://project94-b1972-default-rtdb.firebaseio.com",
+    projectId: "project94-b1972",
+    storageBucket: "project94-b1972.appspot.com",
+    messagingSenderId: "722492663279",
+    appId: "1:722492663279:web:6ff7d22f6826f01493421a"
+};
+
+firebase.initializeApp(firebaseConfig);
